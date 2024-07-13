b7a907b6b2aab741c5940d425dfca61a70391ea1
Isaac <isaacbalabbo0819@gmail.com>
2024-07-13 17:47:05 +0800

    First Commit & added Featured, Tags, Recent Jobs Section

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="text-sm text-gray-500 font-semibold">GovExec</a>
+        <h3 class="text-xl font-bold mt-2 group-hover:text-blue-800 transition-colors duritation-300">Full stack Laravel Developer</h3>
+        <p class="text-sm text-gray-500 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex flex-col">
+        <div>
+            <x-tag href="#">Tag</x-tag>
+            <x-tag href="#">Tag</x-tag>
+        </div>
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<x-panel>
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold">Video Produces</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag href="#">Tag</x-tag>
+            <x-tag href="#">Tag</x-tag>
+            <x-tag href="#">Tag</x-tag>
+        </div>
+        <x-employer-logo width="42" />
+    </div>
+</x-panel>
